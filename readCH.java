8bb0ef2a8653a92fda98b3180de0dcb80335f28e
Shivam Sharma <shivamkumarsasaram2005@gmail.com>
2025-12-28 16:00:43 -0800

    read CHAR

---
+++ readCH.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class readCH {
+    public static void main(String[] args) {
+    
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the charecter");
+        char ch=sc.next().charAt(3);
+        System.out.println("read charecter =" +ch);
+    }
+}
