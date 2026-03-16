d51af553ad3f31589030de5d09da86271aa27c15
Shivam Sharma <shivamkumarsasaram2005@gmail.com>
2026-03-16 02:40:24 -0700

    java swapNumber

---
+++ swapNumber.java
@@ -0,0 +1,13 @@
+public class swapNumber {
+    public static void main(String args[]){
+        int a=5;
+        int b=10;
+      // System.out.println("before swap a= "+a+" b= "+b);
+        int temp=a;
+        a=b;
+        b=temp;
+       // System.out.println("after swap a= "+a+" b= "+b);
+       System.out.println("swap number a="+a);
+       System.out.println("swap number b =" +b);
+    }
+}
