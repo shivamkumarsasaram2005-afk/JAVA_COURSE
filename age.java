c9e86e333a4bf42c5dcc72bd97575c696a1c3f9c
Shivam Sharma <shivamkumarsasaram2005@gmail.com>
2026-04-13 05:52:51 -0700

    First Java program

---
+++ age.java
@@ -0,0 +1,13 @@
+public class age {
+    public static void main(String args[]){
+        int age=25;
+        if(age>=18){
+            System.err.println("you are eligible to drive");
+        
+        }else{
+                System.err.println("you are not eligible to drive");
+            }
+        
+        }
+    }
+
